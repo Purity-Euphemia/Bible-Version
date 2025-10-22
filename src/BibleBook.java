a1163d78f0cf29547deb0882157514cd69f5173b
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-10-22 20:31:07 +0100

    first commit

--- src/BibleBook.java
+++ src/BibleBook.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibleBook {
+    private String name;
+    private int number;
+    private String shortName;
+    private List<Chapter> chapters;
+
+    public BibleBook(int number, String name, String shortName){
+        this.number = number;
+        this.name = name;
+        this.shortName = shortName;
+        this.chapters = new ArrayList<>();
+    }
+    public void addChapter(Chapter chapter) {
+        chapters.add(chapter);
+    }
+    public int getNumber() {
+        return number;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getShortName() {
+        return shortName;
+    }
+    public List<Chapter> getChapters() {
+        return chapters;
+    }
 }
